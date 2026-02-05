fb8dfbbc35ecd30c64d02589d0dcaae5ac24c431
Sebas <sebastian.lara6087@alumnos.udg.mx>
2026-02-04 19:23:06 -0600

    resized main window

--- src/RiseEngineCore/Main.cpp
+++ src/RiseEngineCore/Main.cpp
@@ -1,8 +1,9 @@
 #include <RiseEngineCore/Application.h>
 
+
 int main()
 {
-	Application application(1600, 900, "Rise Engine");
+	Application application(800, 600, "Rise Engine");
 	application.Run();
 
 
