d567d98d85c90716411432893a2299e62598d303
Sebastian L <sebastian.lara6087@alumnos.udg.mx>
2026-05-30 16:25:20 -0600

    entity class

---
+++ src/CommonFramework/WorldObjects/Entity.cpp
@@ -0,0 +1,32 @@
+#include "Entity.hpp"
+#include <CommonFramework/Components/TransformComponent.hpp>
+
+using Entity = RiseEngine::CommonFramework::Entity;
+
+Entity::Entity(const std::string& name) : name_(name)
+{
+	rootComponent_ = AddComponent<TransformComponent>("RootComponent");
+}
+
+void Entity::Update(f32 deltaTime)
+{
+	if (!bActive_ || !bUpdateComponentsEnabled_) return;
+
+	// Update all components.
+	for (const auto& component : components_)
+	{
+		if (component->IsActive())
+		component->Update(deltaTime);
+	}
+}
+
+void Entity::RemoveComponent(EntityComponent* component)
+{
+	if (component == rootComponent_) return;
+
+	component->OnDetach();
+	std::erase_if(components_, [component](const std::unique_ptr<EntityComponent>& ec)
+		{
+			return ec.get() == component;
+		});
+}
---
+++ src/CommonFramework/WorldObjects/Entity.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <ComonFramework/Components/EntityComponent.hpp>
+#include <CommonFramework/Components/TransformComponent.hpp>
+#include <unordered_set>
+
+namespace RiseEngine::CommonFramework
+{
+
+	// Base class for all entities in the world.
+	// Always has a Root Component (TransformComponent)
+	// that defines its position in the world.
+	class Entity
+	{
+	public:
+		explicit Entity(const std::string& name = "Entity");
+		virtual ~Entity() = default;
+
+		virtual void Update(f32 deltaTime);
+		// Tags.
+		inline void AddTag(const std::string& tag) { tags_.insert(tag); }
+		inline void RemoveTag(const std::string& tag) { tags_.erase(tag); }
+		inline bool HasTag(const std::string& tag) const { return tags_.contains(tag); }
+		inline const std::unordered_set<std::string>& GetTags() const { return tags_; }
+
+		// Components.
+		template<typename T, typename... Args>
+		T* AddComponent(Args&&... args)
+		{
+			static_assert(std::is_base_of_v<EntityComponent, T>, "T must be a subclass of EntityComponent");
+			
+			auto component = std::make_unique<T>(std::forward<Args>(args)...);
+			T* ptr = component.get();
+			ptr->SetOwner(this);
+			ptr->OnAttach();
+			components_.emplace_back(std::move(component));
+			return ptr;
+		}
+
+		template<typename T>
+		T* GetComponent() const
+		{
+			for (const auto& component : components_)
+			{
+				if (T* casted = dynamic_cast<T*>(component.get()))
+				{
+					return casted;
+				}
+			}
+			return nullptr;
+		}
+
+		template<typename T>
+		bool HasComponent() const
+		{
+			return GetComponent<T>() != nullptr;
+		}
+
+		void RemoveComponent(EntityComponent* component);
+
+		// Getters.
+		const std::string& GetName() const { return name_; }
+		TransformComponent* GetRootComponent() const { return rootComponent_; }
+		bool IsActive() const { return bActive_; }
+		void SetActive(const bool active) { bActive_ = active; }
+
+	private:
+		std::string name_;
+		bool bActive_ = true;
+		bool bUpdateComponentsEnabled_ = false;
+		TransformComponent* rootComponent_ = nullptr;
+		std::vector<std::unique_ptr<EntityComponent>> components_;
+		std::unordered_set<std::string> tags_;
+	};
+}
