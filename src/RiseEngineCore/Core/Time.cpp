f3c3c290c5d12171df0885c30976d15cbf35f0a3
Sebastian L <sebastian.lara6087@alumnos.udg.mx>
2026-06-10 18:34:15 -0600

    fixed uninitialized variables

--- src/RiseEngineCore/Core/Time.cpp
+++ src/RiseEngineCore/Core/Time.cpp
@@ -1,7 +1,7 @@
 #include "Time.hpp"
 
 f64 RiseEngine::Time::s_deltaTime_ = 0.0;
-f64 RiseEngine::Time::s_deltaTime_ = 0.0;
+f64 RiseEngine::Time::s_totalTime_ = 0.0;
 uint64 RiseEngine::Time::s_frameCount_ = 0;
 std::chrono::steady_clock::time_point RiseEngine::Time::s_lastFrame_ = std::chrono::steady_clock::now();
 std::chrono::steady_clock::time_point RiseEngine::Time::s_startTime_ = std::chrono::steady_clock::now();
@@ -11,6 +11,7 @@ void RiseEngine::Time::Update()
 	auto now = std::chrono::steady_clock::now();
 	std::chrono::duration<f64> elapsed = now - s_lastFrame_;
 	s_lastFrame_ = now;
+	s_deltaTime_ = elapsed.count();
 	s_totalTime_ += s_deltaTime_;
 	s_frameCount_++;
 }
